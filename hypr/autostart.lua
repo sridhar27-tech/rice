99596c5cf82f3bc9b5c63897d8a813d57eed824c
lightning4477 <vignesh112847@gmail.com>
2026-07-04 23:08:15 +0530

    feat: Configure Waybar with Rose Pine Moon theme

--- hypr/autostart.lua
+++ hypr/autostart.lua
@@ -4,11 +4,9 @@
 
 -- See https://wiki.hypr.land/Configuring/Basics/Autostart/
 
--- Autostart necessary processes (like notifications daemons, status bars, etc.)
--- Or execute your favorite apps at launch like this:
---
--- hl.on("hyprland.start", function () 
---   hl.exec_cmd(terminal)
---   hl.exec_cmd("nm-applet")
---   hl.exec_cmd("waybar & hyprpaper & firefox")
--- end)
+hl.on("hyprland.start", function ()
+    hl.exec_cmd("waybar")
+    hl.exec_cmd("swaync")
+    hl.exec_cmd("hyprpaper")
+    hl.exec_cmd("hypridle")
+end)
