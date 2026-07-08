12c5f2c93c8e7d5a93a6bf20769cc436e4dcfe8a
lightning4477 <vignesh112847@gmail.com>
2026-07-08 10:05:36 +0530

    set the refreash rate to 60hz

--- hypr/monitors.lua
+++ hypr/monitors.lua
@@ -3,9 +3,10 @@
 -- ------------------
 
 -- See https://wiki.hypr.land/Configuring/Basics/Monitors/
+
 hl.monitor({
-    output   = "",
-    mode     = "preferred",
-    position = "auto",
-    scale    = "auto",
+    output   = "eDP-1",
+    mode     = "1920x1080@60",
+    position = "0x0",
+    scale    = 1,
 })
